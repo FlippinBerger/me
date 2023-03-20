f06a19d5c99d4211578ce976e61bc594ef2af802
flippin <flippinberger@gmail.com>
2023-03-19 19:49:26 -0600

    Added under construction content.

--- web/src/Blog.tsx
+++ web/src/Blog.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Blog = () => {
     return (
         <div>
-            Welcome to the tech blog
+            Welcome to the tech blog, which is under construction.
         </div>
     )
 }
