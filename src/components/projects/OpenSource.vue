463cbd958dcb49ddc5986d8dfcdaa6489d3ba09a
Chris Berger <flippinberger@gmail.com>
2024-04-19 11:13:03 -0600

    Add Bevy info to portfolio

--- src/components/projects/OpenSource.vue
+++ src/components/projects/OpenSource.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>Welcome to the open source projects</h1>
+    <div class='wrapper'>
+        <a class='title-link' target='_blank' href='https://github.com/bevyengine/bevy'>Bevy</a>
+        <h3>This project was my first real foray into open source development and my first Rust code that hit
+            prod!<br />
+            The changes aren't that impressive, but I'm still following along on Discord and hope to make some more
+            meaningful commits someday soon.<br />Links to commits:
+        </h3>
+        <a target='_blank'
+            href='https://github.com/bevyengine/bevy/pull/9204/commits/be91b645d814c16335f50b51e12a3e31aef2ad6c'>Add Has
+            World Query to bevy_ecs::prelude</a><br />
+        <a target='_blank'
+            href='https://github.com/bevyengine/bevy/pull/9293/commits/6ec31219ae475ecd987af15462c829c963be7ea3'>Derive
+            Debug for ManualEventIterator</a>
+    </div>
 </template>
+
+<style scoped>
+.title-link {
+    font-size: 32px;
+}
+
+.wrapper {
+    width: 75%;
+}
+</style>
