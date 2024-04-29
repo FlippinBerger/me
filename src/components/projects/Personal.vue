b9b9c8ebaa2f93b3e7fdb4a2b707a90b029b158b
Chris Berger <flippinberger@gmail.com>
2024-04-29 14:57:30 -0600

    Add DJmocracy to personal projects

--- src/components/projects/Personal.vue
+++ src/components/projects/Personal.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>Welcome to the personal projects</h1>
-    </template>
+    <div>
+        <ul>
+            <li>
+                <a target='_blank' href='https://github.com/flippinberger/djmocracy'>
+                    <h2>DJmocracy (Work in Progress)</h2>
+                </a>
+                <p>This is a personal project that I thought of back in 2016 and never got around to building. Until now.</p>
+                <p>I'm building it with a Vue frontend, and a Go backend with Mongodb</p>
+                <p>DJmocracy is crowd sourced playlist generation that makes amateur DJs and house party hosts lives easier</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    h2 {
+        margin-top: 8px;
+    }
+
+    a {
+        margin: 8px 0;
+        padding: 8px 0;
+    }
+</style>
